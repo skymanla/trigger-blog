a6775d271b3bcd8c8a737a763ebdb3ed77d4cd56
김남태 <ryan@thebiz.kr>
2022-12-26 17:09:13 +0900

    index footer 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,20 +21,7 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </main>      
     </div>
   )
 }
