5bbdc2f45424830a015693b56df568a38fb51350
ryan-dev <ryan-dev@kakao.com>
2022-12-31 21:18:31 +0900

    index 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import KeenSlider from '../components/keenSlider'
-import {CssBaseline} from "@mui/material";
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { deepOrange, deepPurple } from '@mui/material/colors';
+import BlogPostCard from "../components/BlogPostCard";
+import React from "react";
+import Link from 'next/link'
 
-// const Home: NextPage = () => {
-//     return (
-//     <div className={styles.container}>
-//         <Head>
-//             <title>Welcome Trigger.kr</title>
-//             <meta name="description" content="트리거 홈페이지에 오신 걸 환영합니다" />
-//             <link rel="icon" href="/favicon.ico" />
-//         </Head>
-//
-//         <KeenSlider />
-//         {/*<SwiperSlider />*/}
-//     </div>
-//     )
-// }
 
 const Home: NextPage = () => {
     return (
@@ -30,13 +13,48 @@ const Home: NextPage = () => {
                 <meta name="description" content="트리거 홈페이지에 오신 걸 환영합니다" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <CssBaseline>
-                <Stack direction="row" spacing={2}>
-                    <Avatar>H</Avatar>
-                    <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
-                    <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
-                </Stack>
-            </CssBaseline>
+            <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
+                Featured Posts
+            </h3>
+            <div className="flex gap-6 flex-col md:flex-row">
+                <BlogPostCard
+                    title="트리거에 오신 것을 환영합니다"
+                    slug="hello-world"
+                    gradient="from-[#D8B4FE] to-[#818CF8]"
+                />
+                {/*<BlogPostCard*/}
+                {/*    title="Rust Is The Future of JavaScript Infrastructure"*/}
+                {/*    slug="rust"*/}
+                {/*    gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"*/}
+                {/*/>*/}
+                {/*<BlogPostCard*/}
+                {/*    title="Past, Present, and Future of React State Management"*/}
+                {/*    slug="react-state-management"*/}
+                {/*    gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"*/}
+                {/*/>*/}
+            </div>
+            <Link
+                href="/blog"
+                className="flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+            >
+                <>
+                    {'Read all posts'}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="h-6 w-6 ml-1"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                        />
+                    </svg>
+                </>
+            </Link>
         </>
     )
 }
