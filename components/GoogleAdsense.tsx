0a8ffd42ccb245b72cc0c657d8ddb1df063d7c6d
김남태 <ryan@thebiz.kr>
2023-01-04 17:56:50 +0900

    env 로 분기쳐서 애드센스 타게 변경

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -4,15 +4,18 @@ import React from "react";
 const GoogleAdsense = () => {
     return (
         <>
-            <Script
-                id="Adsense-id"
-                data-ad-client="ca-pub-1586372003132738"
-                async
-                strategy="afterInteractive"
-                onError={ (e) => { console.error('Script failed to load', e) }}
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                crossOrigin="anonymous"
-            />
+            {process.env.APP_ENV === "production" ?
+                <Script
+                    id="Adsense-id"
+                    data-ad-client="ca-pub-1586372003132738"
+                    async
+                    strategy="afterInteractive"
+                    onError={ (e) => { console.error('Script failed to load', e) }}
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                    crossOrigin="anonymous"
+                />
+                : <script>console.log(12345)</script>
+            }
         </>
     )
 }
