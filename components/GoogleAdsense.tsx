1f62d338a267aae166b4f036399b3d343beb0712
김남태 <ryan@thebiz.kr>
2023-01-05 11:07:21 +0900

    운영 에러나서 env 제거

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -4,18 +4,15 @@ import React from "react";
 const GoogleAdsense = () => {
     return (
         <>
-            {process.env.APP_ENV === "production" ?
-                <Script
-                    id="Adsense-id"
-                    data-ad-client="ca-pub-1586372003132738"
-                    async
-                    strategy="afterInteractive"
-                    onError={ (e) => { console.error('Script failed to load', e) }}
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                    crossOrigin="anonymous"
-                />
-                : <script>console.log(12345)</script>
-            }
+            <Script
+                id="Adsense-id"
+                data-ad-client="ca-pub-1586372003132738"
+                async
+                strategy="afterInteractive"
+                onError={ (e) => { console.error('Script failed to load', e) }}
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                crossOrigin="anonymous"
+            />
         </>
     )
 }
