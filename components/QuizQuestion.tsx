ce7ce1411227e90729f1fd38c7125678f9de1bf8
ryan-dev <ryan-dev@kakao.com>
2025-03-09 11:11:15 +0900

    Feat: numbering

--- components/QuizQuestion.tsx
+++ components/QuizQuestion.tsx
@@ -12,13 +12,13 @@ const QuizQuestion: React.FC<QuizQuestionProps> = ({ quizItem, onSelectAnswer })
     <div className="mb-8">
       <h2 className="text-xl font-bold mb-4 dark:text-white">{quizItem.question}</h2>
       <ul className="space-y-2">
-        {quizItem.options.map((option) => (
+        {quizItem.options.map((option, index) => (
           <li key={option}>
             <button
               className="w-full p-3 bg-gray-100 hover:bg-gray-200 rounded text-left dark:bg-gray-800 dark:hover:bg-gray-700"
               onClick={() => onSelectAnswer(option)}
             >
-              {option}
+              {index+1}. {option}
             </button>
           </li>
         ))}
